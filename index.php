488976addcca81a066f1b1e53f50e810ec371965
HLWA007 <chg140100@gmail.com>
2021-09-26 10:40:15 +0530

    Added a carousel and some spaces

--- index.php
+++ index.php
@@ -18,17 +18,17 @@ include 'includes/header.php'?>
     <a class="btn btn-success" href="https://momlovesbest.com/baby-shower-songs" role="button">Celebrate</a>
     <a class="btn btn-dark" href="https://onionplay.uk/" role="button">Watch a Movie</a>
     <a class="btn btn-info" href="https://www.hopscotch.in/baby?q=%7B%22browse%22:%22Girl%27s~~Unisex%22,%22isCategoryPreselected%22:true,%22isFromRefineFilter%22:true%7D" role="button">Clothes for Chinya</a>
-<?php require 'includes/footer.php'?>
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <br/>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="C:\Users\Dell\Pictures\Wall-E\Mirandus_Teaser" class="d-block w-100" alt="rouge3">
+      <img src="Mirandus_Teaser.jpg" class="d-block w-100" alt="rouge3">
     </div>
     <div class="carousel-item">
-      <img src="C:\Users\Dell\Pictures\Wall-E\288747" class="d-block w-100" alt="rouge2">
+      <img src="288747.jpg" class="d-block w-100" alt="rouge2">
     </div>
     <div class="carousel-item">
-      <img src="C:\Users\Dell\Pictures\Wall-E\658842" class="d-block w-100" alt="rouge1">
+      <img src="658842.jpg" class="d-block w-100" alt="rouge1">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
@@ -40,3 +40,5 @@ include 'includes/header.php'?>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
+<br/>
+    <?php require 'includes/footer.php'?>
