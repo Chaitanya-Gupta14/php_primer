922f596645766cba70e20b30226cfd8e6a0af5e5
HLWA007 <chg140100@gmail.com>
2021-09-26 10:21:27 +0530

    Carousel addition

--- index.php
+++ index.php
@@ -19,3 +19,24 @@ include 'includes/header.php'?>
     <a class="btn btn-dark" href="https://onionplay.uk/" role="button">Watch a Movie</a>
     <a class="btn btn-info" href="https://www.hopscotch.in/baby?q=%7B%22browse%22:%22Girl%27s~~Unisex%22,%22isCategoryPreselected%22:true,%22isFromRefineFilter%22:true%7D" role="button">Clothes for Chinya</a>
 <?php require 'includes/footer.php'?>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="C:\Users\Dell\Pictures\Wall-E\Mirandus_Teaser" class="d-block w-100" alt="rouge3">
+    </div>
+    <div class="carousel-item">
+      <img src="C:\Users\Dell\Pictures\Wall-E\288747" class="d-block w-100" alt="rouge2">
+    </div>
+    <div class="carousel-item">
+      <img src="C:\Users\Dell\Pictures\Wall-E\658842" class="d-block w-100" alt="rouge1">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
