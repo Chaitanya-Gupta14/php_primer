3e03ecb82225f342bc3196ed78772ff35b8c12c1
HLWA007 <chg140100@gmail.com>
2022-01-23 22:37:50 +0530

    Well Organization
    For showcase

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php 
 $title = 'INDEX';
 include 'includes/header.php'?>
- <h1>Hello Pucha!!!!</h1>
+ <h1>Welcome!!!!</h1>
 <br/> 
     <?php
-    echo 'Love ya!!..Stay positive!!';
+    echo 'Stay positive!!';
     echo '<br/>';
-    echo 'I am always gonna be by your side....My baby!!';
+    echo 'You "know" your worth and your capabilities. So believe and take a leap of Faith';
     ?>
 <br/>
     <?php
-    $name = 'chinyaa!!!';
+    $name = 'Determination';
     echo $name;
-    echo '<br/><h1>Our babys name is: '.$name.'</h1><br/>';
+    echo '<br/><h1>The way to success is: '.$name.'</h1><br/>';
     ?>
     <!-- This below is the Button -->
-    <a class="btn btn-success" href="https://momlovesbest.com/baby-shower-songs" role="button">Celebrate</a>
+    <!--<a class="btn btn-success" href="https://momlovesbest.com/baby-shower-songs" role="button">Celebrate</a>-->
     <a class="btn btn-dark" href="https://onionplay.uk/" role="button">Watch a Movie</a>
-    <a class="btn btn-info" href="https://www.hopscotch.in/baby?q=%7B%22browse%22:%22Girl%27s~~Unisex%22,%22isCategoryPreselected%22:true,%22isFromRefineFilter%22:true%7D" role="button">Clothes for Chinya</a>
+    <a class="btn btn-info" href="https://www.hopscotch.in/baby?q=%7B%22browse%22:%22Girl%27s~~Unisex%22,%22isCategoryPreselected%22:true,%22isFromRefineFilter%22:true%7D" role="button">Baby Clothes</a>
     <br/>
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
   <div class="carousel-inner">
